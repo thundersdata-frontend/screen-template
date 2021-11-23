d54410cdae167a9bc8b1c447d03e71b780a8297d
chj_damon <chjdamon@gmail.com>
2021-11-23 16:04:21 +0800

    feat: 初始化项目模板

---
+++ typings.d.ts
@@ -0,0 +1,13 @@
+declare module '*.css';
+declare module '*.less';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+
+declare module '*.svg' {
+  export function ReactComponent(
+    props: React.SVGProps<SVGSVGElement>,
+  ): React.ReactElement;
+  const url: string;
+  export default url;
+}
